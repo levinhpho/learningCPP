9f5011de31757c656aded5623893efbe945b37ed
levinhpho <levinhpho@gmail.com>
2022-02-25 23:09:10 -0800

    phuong trinh bac hai

--- Bai2.cpp
+++ Bai2.cpp
@@ -1,33 +1,51 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    int num1, num2, num3;
+      int num1, num2, num3;
     std::cin>>num1;
     std::cin>>num2;
     std::cin>>num3;
     
+    if(num1<num2 && num2<num3)
+{   
+    std::cout<<num1;
+    std::cout<<num2;
+    std::cout<<num3;
+}
+    if (num1<num3 && num3<num2)
+{    
+    std::cout<<num1;
+    std::cout<<num3;
+    std::cout<<num2;
+}
+    if (num2<num3 && num3<num1)
 {
-	if(num1<num2 && num2<num3);
-  
-    std::cout<<num1;
-    std::cout<<num2;
-    std::cout<<num3;
-}
- {
-	if (num1<num3 && num3<num2);
-    
-    std::cout<<num1;
-    std::cout<<num3;
-    std::cout<<num2;
-}
-   {
-	 if (num2<num3 && num3<num1);
-	 
     std::cout<<num2;
     std::cout<<num3;
     std::cout<<num1;
 }
- return 0;
+    if (num2<num1 && num1<num3)
+{
+    std::cout<<num2;
+    std::cout<<num1;
+    std::cout<<num3;
 }
+    if (num3<num1 && num1<num2)
+{
+    std::cout<<num3;
+    std::cout<<num1;
+    std::cout<<num2;
+}
+    if (num3<num2 && num2<num1)
+{
+    std::cout<<num3;
+    std::cout<<num2;
+    std::cout<<num1;
+}
+    return 0;
+}
+
+
+
 
